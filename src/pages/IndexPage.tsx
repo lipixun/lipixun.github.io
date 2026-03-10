926e574988059d91d8e3c6b26de07c10a734b0cd
lipixun <lipixun@gmail.com>
2026-03-10 19:44:40 +0800

    update

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,6 +1,7 @@
 // IndexPage
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
+import type { CSSProperties } from '@mui/material/styles';
 
 export function IndexPage() {
   return (
@@ -17,11 +18,16 @@ export function IndexPage() {
     >
       <Stack spacing='0.5em' alignItems='center'>
         <Avatar alt='Avatar' src='https://github.com/lipixun.png?size=96' sx={{ width: 96, height: 96 }} />
-        <span>Simon</span>
-        <span>Intangible Cultural Heritage Inheritor of Hand-Crafted Coding</span>
-        <span>匠心 · 纯手工</span>
-        <span>Beijing China</span>
+        <span style={textStyle}>Simon</span>
+        <span style={textStyle}>Intangible Cultural Heritage Inheritor of Hand-Crafted Coding</span>
+        <span style={textStyle}>匠心 · 纯手工</span>
+        <span style={textStyle}>Beijing China</span>
       </Stack>
     </div>
   );
+}
+
+const textStyle: CSSProperties = {
+  '-webkit-text-stroke': '1em black',
+  paintOrder: 'stroke fill',
 }
